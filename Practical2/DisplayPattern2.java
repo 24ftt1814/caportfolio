100c728d8607d7831cfed9e9926086907e99325e
24ftt1814 <24FTT1814@student.pb.edu.bn>
2025-08-12 20:39:35 +0800

    Add files via upload

---
+++ Practical2/DisplayPattern2.java
@@ -0,0 +1,17 @@
+public class DisplayPattern2 {
+    public static void main (String []args){
+        System.out.println("    .=' ' ' '=.");
+        System.out.println("   /           \\");
+        System.out.println("  |  o       o |");
+        System.out.println("  \\     /\\    /");
+        System.out.println("   /'--=\\/=--'\\");
+        System.out.println("  / /        \\ \\");
+        System.out.println("  \\/\\        /\\/        CHIP CHIP!");
+        System.out.println("     '.    .' ");
+        System.out.println("      /|\\ /|\\");
+
+
+
+
+    }
+}
---
+++ Practical2/DisplayTwoMessages.java
@@ -0,0 +1,6 @@
+public class DisplayTwoMessages {
+     public static void main(String[] args) {
+        System.out.println("Welcome to AD2204 Introduction to Programming\nThis is Practical 2");
+       // System.out.println("This is Practical 2");
+    }
+}
